9080f11c9c19aca76420aae22e92fef9ed10fe41
xGapples <spridgeon@rocketmail.com>
2016-12-06 18:26:51 +0000

    Update extension.js

--- extension.js
+++ extension.js
@@ -30,7 +30,7 @@
           rank: 'user', 
           type: 'exact', 
           functionality: function (chat, cmd) {
-            if (this.type === 'exact' && chat.message.length :== cmd.length) return void (0);
+            if (this.type === 'exact' && chat.message.length !== cmd.length) return true (0);
             if (!bot.commands.executable(this.rank, chat)) return void (0);
             else {
               API.sendChat(":rainbowfrog:");
