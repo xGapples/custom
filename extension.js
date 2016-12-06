a7964e6fa03c6613411d2defa30bf6db96954104
xGapples <spridgeon@rocketmail.com>
2016-12-06 18:25:11 +0000

    Update extension.js

--- extension.js
+++ extension.js
@@ -25,12 +25,12 @@
         }
 
         // Example code for a bot command:
-        bot.commands.baconCommand = {
-          command: ':rainbowfrog:',  // The command to be called. With the standard command literal this would be: !bacon
-          rank: 'user', // Minimum user permission to use the command
-          type: 'exact', // Specify if it can accept variables or not (if so, these have to be handled yourself through the chat.message
+        bot.commands.rainbowfrogCommand = {
+          command: ':rainbowfrog:',  
+          rank: 'user', 
+          type: 'exact', 
           functionality: function (chat, cmd) {
-            if (this.type === 'exact' && chat.message.length !== cmd.length) return void (0);
+            if (this.type === 'exact' && chat.message.length :== cmd.length) return void (0);
             if (!bot.commands.executable(this.rank, chat)) return void (0);
             else {
               API.sendChat(":rainbowfrog:");
